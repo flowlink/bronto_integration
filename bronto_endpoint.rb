efa9ab78955f97bb36d72eea32d59676931ea32a
Washington Luiz <huoxito@gmail.com>
2014-08-30 00:32:22 -0300

    bit better notification message

--- bronto_endpoint.rb
+++ bronto_endpoint.rb
@@ -18,14 +18,14 @@ class BrontoEndpoint < EndpointBase::Sinatra::Base
   ["/add_order", "/update_order"].each do |path|
     post path do
       BrontoIntegration::Order.new(@config, @payload).create_or_update
-      result 200, "Order info sent to Bronto"
+      result 200, "Order #{@payload[:order][:id]} sent to Bronto"
     end
   end
 
   ["/add_customer", "/update_customer"].each do |path|
     post path do
       BrontoIntegration::Contact.new(@config, @payload).set_up
-      result 200, "Contact info sent to Bronto"
+      result 200, "Contact #{@payload[:customer][:email]} sent to Bronto"
     end
   end
 
